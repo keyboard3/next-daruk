aa5aa598e376ef831855d141dd86853d17a34b39
甘春雨 <keyboard3@icloud.com>
2022-09-12 14:09:42 +0800

    支持github workflow 及 k3s

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+kubectl apply -f deploy/deployment.yml
+kubectl apply -f deploy/service.yml 
--- start.sh
+++
@@ -1,4 +0,0 @@
-git pull
-docker build -t  "next-daruk" .
-kill -9 `lsof -t -i:3003`
-docker run -it -p 3003:3000 next-daruk
